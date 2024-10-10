c46aff4bf105337d487955a2a8d18f4b590ad1de
sfr71 <sfr71@uclive.ac.nz>
2024-10-11 01:22:54 +1300

    Color scheme fix

--- app/src/main/java/nz/ac/canterbury/seng303/betzero/screens/EmergencyScreen.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/betzero/screens/EmergencyScreen.kt
@@ -3,43 +3,45 @@ package nz.ac.canterbury.seng303.betzero.screens
 import android.widget.Toast
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 import nz.ac.canterbury.seng303.betzero.viewmodels.EmergencyViewModel
 import nz.ac.canterbury.seng303.betzero.viewmodels.SlotShape
 import org.koin.androidx.compose.koinViewModel
-import androidx.compose.ui.window.DialogProperties
-import androidx.compose.material3.Surface
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AttachMoney
-import androidx.compose.material3.Icon
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun EmergencyScreen(navController: NavController, viewModel: EmergencyViewModel = koinViewModel()) {
@@ -90,7 +92,7 @@ fun SlotMachinePopup(onClose: () -> Unit, viewModel: EmergencyViewModel) {
             modifier = Modifier
                 .fillMaxSize(0.9f)
                 .padding(16.dp),
-            color = Color.White,
+            color = MaterialTheme.colorScheme.surface,
             shape = RoundedCornerShape(16.dp)
         ) {
             Box(modifier = Modifier.fillMaxSize()) {
